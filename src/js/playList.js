9420c1a3c1689bc94aaf751fe57c9718ce47c7cd
dima92 <dima-tkachenko@mail.ru>
2021-07-20 21:23:58 +0300

    feat: final ended task a momentum

---
+++ src/js/playList.js
@@ -0,0 +1,22 @@
+const playList = [
+  {
+    title: 'Aqua Caelestis',
+    src: 'http://127.0.0.1:8887/Aqua Caelestis.mp3',
+    duration: '00:58'
+  }, {
+    title: 'River Flows In You',
+    src: 'http://127.0.0.1:8887/River Flows In You.mp3',
+    duration: '03:50'
+  },
+  {
+    title: 'Summer Wind',
+    src: 'http://127.0.0.1:8887/Summer Wind.mp3',
+    duration: '05:05'
+  },
+  {
+    title: 'Ennio Morricone',
+    src: 'http://127.0.0.1:8887/Ennio Morricone.mp3',
+    duration: '05:03'
+  }
+]
+export default playList;
